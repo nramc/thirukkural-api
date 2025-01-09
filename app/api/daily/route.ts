0be674876acd83adfb81da758adb0785a173f0dc
Ram <ramachandrannellai@gmail.com>
2025-01-09 09:36:53 +0100

    feat: kural of the day api updated with cors headers to allow all origins

--- app/api/daily/route.ts
+++ app/api/daily/route.ts
@@ -1,11 +1,8 @@
 import kuralService from "@/app/service/KuralService";
 
 export async function GET() {
-    // Add CORS headers
     const headers = {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
     };
 
     return Response.json(kuralService.kuralOfTheDay(), {
