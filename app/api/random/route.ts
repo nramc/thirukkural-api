860f405cded741574a46a5d555e9c3dc9134486f
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2025-11-19 17:39:51 +0100

    chore: add chapter-based filtering for random Kural retrieval

--- app/api/random/route.ts
+++ app/api/random/route.ts
@@ -3,13 +3,18 @@ import randomKuralService from "@/app/service/random-kural-service";
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
-    const sectionId = searchParams.get('section');
 
+    // when chapter number provided, return random kural from the chapter
+    const chapterId = searchParams.get('chapter');
+    if (chapterId && !Number.isNaN(Number.parseInt(chapterId, 10))) {
+        return NextResponse.json(randomKuralService.getRandomKuralByChapter(Number.parseInt(chapterId, 10)));
+    }
+    
     // when section number provided, return random kural from the section
+    const sectionId = searchParams.get('section');
     if (sectionId && ['1', '2', '3'].includes(sectionId)) {
         return NextResponse.json(randomKuralService.getRandomKuralBySection(Number.parseInt(sectionId, 10)));
     }
-
 
     // Get random kural
     return NextResponse.json(randomKuralService.getRandomKural());
