65c757491533447a86bbe3ea4ede74ee2ad88f4c
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2026-08-30 20:58:50 +0200

    feat: implement search functionality for Kural by keywords with pagination

---
+++ app/api/kural/route.ts
@@ -0,0 +1,15 @@
+import kuralService from "@/app/service/kural-service";
+
+export async function GET(request: Request) {
+    const searchParams = new URL(request.url).searchParams;
+
+    // look query param q to filter kural by searching keyword
+    const query = searchParams.get('q');
+    const page = searchParams.get('page') ? Number.parseInt(searchParams.get('page') as string, 10) : 1;
+    const limit = searchParams.get('limit') ? Number.parseInt(searchParams.get('limit') as string, 10) : 10;
+
+    const result = kuralService.searchByKeyword(query ? query.split(',') : [], page, limit);
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
